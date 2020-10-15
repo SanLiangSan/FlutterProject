a3b3eb0736a28791720d6669156a42c678503b7b
xuxingling <xuxingling2020@gmail.com>
2020-10-15 11:44:39 +0800

    add first flutter demo

---
+++ flutter_demo/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/listview.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('Flutter Demo'),
+      ),
+      body: ListViewDemo(),
+    );
+  }
+}
